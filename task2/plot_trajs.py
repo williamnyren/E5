f5f29d73207091f73eec65fb498f7938328dd996
Fredrik Öhrlund <frederi@student.chalmers.se>
2021-12-14 10:55:15 +0100

    Update plot_trajs.py

--- task2/plot_trajs.py
+++ task2/plot_trajs.py
@@ -23,13 +23,17 @@ fnames_low = ['0_low.dat', '1_low.dat', '2_low.dat', '3_low.dat', '4_low.dat']
 # Read normal gaussian from results.dat
 data_high = []
 data_low = []
+time_avg_high = np.zeros(np.genfromtxt(file_path + fnames_high[0], delimiter=',', skip_header=1).shape)
+time_avg_low = np.zeros(np.genfromtxt(file_path + fnames_low[0], delimiter=',', skip_header=1).shape)
 
 fig, ax = plt.subplots(nrows=4, ncols=1, sharex=True, figsize=(16, 16))
 dt = 0.001
 for i, fname_high in enumerate(fnames_high):
     data_high = np.genfromtxt(file_path + fnames_high[i], delimiter=',', skip_header=1)
+    time_avg_high += data_high
 
     data_low = np.genfromtxt(file_path + fnames_low[i], delimiter=',', skip_header=1)
+    time_avg_low += data_low
 
     t = np.arange(len(data_low[:, 0]))*dt
     ax[0].plot(t, data_low[:, 0])
@@ -39,6 +43,8 @@ for i, fname_high in enumerate(fnames_high):
     t = np.arange(len(data_high[:, 0]))*dt
     ax[2].plot(t, data_high[:, 0])
     ax[3].plot(t, data_high[:, 1])
+time_avg_low = time_avg_low/5.0
+time_avg_high = time_avg_high/5.0
 ax[0].set_xlim((0, 6))
 ax[0].set_ylabel(r"r_low []")
 ax[1].set_xlim((0, 6))
@@ -48,9 +54,16 @@ ax[2].set_ylabel(r"r_high []")
 ax[3].set_xlim((0, 6))
 ax[3].set_ylabel(r"v_high []")
 ax[3].set_xlabel(r"time [ms]")
+
 plt.savefig('traj.png', dpi=600)
 plt.show()
-
+fig, ax = plt.subplots(ncols=4,nrows=1,figsize=(16,16))
+ax[0].plot(t,time_avg_low[:,0],label='time avg low')
+ax[1].plot(t,time_avg_low[:,1],label='time avg low')
+ax[0].plot(t,time_avg_high[:,0],label='time avg low')
+ax[1].plot(t,time_avg_high[:,1],label='time avg low')
+plt.savefig('time_avg_traj.png',dpi=600)
+plt.show()
 
 # %%
 
